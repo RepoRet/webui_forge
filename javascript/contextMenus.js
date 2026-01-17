5bc21b42b54479afcf912e41c29813594dd4fab2
RepoRet <sm.git@pm.me>
2026-01-16 20:24:38 -0800

    Modified contextMenu
    Adds extends wait duration (5 mins) between infinite generation.

--- javascript/contextMenus.js
+++ javascript/contextMenus.js
@@ -137,7 +137,7 @@ var regen_img2img = null;
 					interrupt.style.display = 'block';
 				}
 			},
-			500);
+			180000);
 		}
     };
     appendContextMenuOption('#txt2img_generate', 'Generate forever', generateOnRepeat_txt2img);
