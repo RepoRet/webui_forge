81e11be8ed906931d8b7e7ff263537676c26e5fe
RepoRet <sm.git@pm.me>
2026-01-18 06:52:32 -0800

    Update COMMANDLINE_ARGS for webui.py
    Added preset parameters / flags for low vram support

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,8 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+#export COMMANDLINE_ARGS="--always-offload-from-vram --cuda-stream --pin-shared-memory --medvram --listen --xformers --o>
+#--medvram --enable-insecure-extension-access"
 
 # python3 executable
 #python_cmd="python3"
